45f51ebebecca574b304fa291d5b8d80d568dd20
liangce@58ganji.com <liangce@58ganji.com>
2016-10-11 15:07:06 +0800

    Initial Commit

---
+++ KVO_Simulate/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  KVO_Simulate
+//
+//  Created by liangce on 16/10/11.
+//  Copyright © 2016年 liangce. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
